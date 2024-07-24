9920c04cd8cc8a432d8f4cdf14c26259397c622c
briesenberg07 <briesenb@uoregon.edu>
2024-07-23 17:45:22 -0700

    iterate SPARQL using f-string @briesenberg07

--- nosatsu_queries.py
+++ nosatsu_queries.py
@@ -1,36 +1,39 @@
 from rdflib import Graph
+from datetime import datetime
 
 # ! to do: Get OD ID for properties - this is what staff will see on spreadsheet
 descriptive_props = [
     "http://purl.org/dc/terms/title",
-    "http://opaquenamespace.org/ns/vra_measurements",
-    "http://id.loc.gov/vocabulary/relators/rps"
+    "http://purl.org/dc/terms/alternative",
+    "http://purl.org/dc/terms/description",
+    "http://opaquenamespace.org/ns/vra_measurements"
+    # ... 
 ]
 
 nosatsu = Graph().parse("../data_scratch/colls/nosatsu.jsonld")
 
 with open("/mnt/c/Users/briesenb/Desktop/nosatsu_report.md", "w+") as mkdown:
     mkdown.write("# Nosatsu metadata overview\n")
-    mkdown.write("## Descriptive properties\n")
+    mkdown.write(f"**Last updated {datetime.today().strftime('%Y-%m-%d')}**\n")
+    mkdown.write("## Descriptive properties which expect a text value\n")
+    mkdown.write("*Is this a complete list??*\n")
 
     for property in descriptive_props:
         toc_entry = f"- {property.split('/')[-1]}: [for items](#{property.split('/')[-1]}---for-items) / [for complex objects](#{property.split('/')[-1]}---for-complex-objects)\n"
         mkdown.write(toc_entry)
 
-    # I think double-curlies would work as well
-    item_q = """
-    SELECT ?value 
-    WHERE {
-        ?item <property_here> ?value .
-        FILTER NOT EXISTS { ?item <info:fedora/fedora-system:def/model#hasModel> 'Generic'  . }
-    }
-    """
-    # ! results are only for the first property in descriptive_props
-    # ! whhhhyyyyyy
+    
     mkdown.write("## Values by property - items\n")
     for property in descriptive_props:
         mkdown.write(f"### {property.split('/')[-1]} - for items\n")
-        item_q = item_q.replace("property_here", property)
+        # I think double-curlies would work as well
+        item_q = f"""
+        SELECT DISTINCT ?value 
+        WHERE {{
+            ?item <{property}> ?value .
+            FILTER NOT EXISTS {{ ?item <info:fedora/fedora-system:def/model#hasModel> 'Generic'  . }}
+        }}
+        """
         result = nosatsu.query(item_q)
         for row in result:
             mkdown.write(f"- {row.value}\n")
