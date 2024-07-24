ace3e79b58a623934cf5482b7918f91c7aeebfb9
briesenberg07 <briesenb@uoregon.edu>
2024-07-23 17:49:42 -0700

    @briesenberg07 to-dos

---
+++ check_all_ons_labels.py
@@ -0,0 +1,4 @@
+# get all ONS.org values
+# iterate through these to see which fail when attempting to retrieve label
+    # this is going to take forever... other way to access RDF besides g.parse("rdfurl.nt")?
+# compile list of failures
