d00579adfc9c926e4c1f77afce951bf5f9c04954
briesenberg07 <briesenb@uoregon.edu>
2024-07-26 16:08:13 -0700

    some nosatsu notes

--- nosatsu_query.py
+++ nosatsu_query.py
@@ -1,6 +1,9 @@
 from rdflib import Graph
 from datetime import datetime
 
+# this creates a huge markdown document of questionable usefulness...
+
+
 # ! to do: Get OD ID for properties - this is what staff will see on spreadsheet
 descriptive_props = [
     "http://purl.org/dc/terms/title",
@@ -26,12 +29,27 @@ with open("/mnt/c/Users/briesenb/Desktop/nosatsu_report.md", "w+") as mkdown:
     mkdown.write("## Values by property - items\n")
     for property in descriptive_props:
         mkdown.write(f"### {property.split('/')[-1]} - for items\n")
-        # I think double-curlies would work as well
         item_q = f"""
         SELECT DISTINCT ?value 
         WHERE {{
             ?item <{property}> ?value .
             FILTER NOT EXISTS {{ ?item <info:fedora/fedora-system:def/model#hasModel> 'Generic'  . }}
+        }}
+        """
+        result = nosatsu.query(item_q)
+        for row in result:
+            mkdown.write(f"- {row.value}\n")
+        mkdown.write("\n")
+        mkdown.write("***back to [top](#nosatsu-metadata-overview)***\n")
+    
+    mkdown.write("## Values by property - for complex objects\n")
+    for property in descriptive_props:
+        mkdown.write(f"### {property.split('/')[-1]} - for complex objects\n")
+        item_q = f"""
+        SELECT DISTINCT ?value 
+        WHERE {{
+            ?item <{property}> ?value .
+            FILTER EXISTS {{ ?item <info:fedora/fedora-system:def/model#hasModel> 'Generic'  . }}
         }}
         """
         result = nosatsu.query(item_q)
